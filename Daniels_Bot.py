fd2f50f53df8c44db6170feae4312979b3a3a657
danielpijnenborg <danielpijnenborg@gmail.com>
2023-03-02 14:37:17 +0100

    update scoring and prevent chasing a bot

--- Daniels_Bot.py
+++ Daniels_Bot.py
@@ -2,11 +2,18 @@ import random
 from ..bot_control import Move
 
 class Daniels_Bot:
-    def get_name(self):
+     def get_name(self):
         return "Picasso"
 
     def get_contributor(self):
-        return "Daniel Pijnenborg"
+        return "Daniel"
+        
+    def enemy_on_tile (self, enemies, x,y):
+        for e in enemies:
+            xe = e['position'][0]
+            ye = e['position'][1]
+            if xe == x and ye == y: return 0
+        return 1
         
     def get_bot_score (self,grid,enemy):
         l = grid.shape[0]
@@ -21,15 +28,11 @@ class Daniels_Bot:
         l = grid.shape[0]
         if tile == 0 : return 0.10*l*l         # take empty tile
         elif tile == id: return 1              # take own tile with risk of two bots result in 0
-        elif abs(id - tile) % 3 == 2 : return self.get_bot_score(grid, tile) # take tile from other
-        elif abs(id - tile) % 3 == 1 : return 0.5 * self.get_bot_score(grid, tile) # clear tile from other
-        else : return 2                         # goto other without take with chance of two bots result in 0
+        elif (id - tile) % 3 == 2 : return self.get_bot_score(grid, tile) # take tile from other
+        elif (id - tile) % 3 == 1 : return 0.5 * self.get_bot_score(grid, tile) # clear tile from other
+        else : return 2                        # goto other without take with chance of two bots result in 0
 
     def determine_next_move(self, grid, enemies, game_info):
-        # Check each adjacent tile and see if it can overwrite that
-        # tile by moving there. If it finds one, maybe move there. Otherwise
-        # do a random movement
-
         x = self.position[0]
         y = self.position[1]
         l = grid.shape[0] # always square
@@ -37,7 +40,7 @@ class Daniels_Bot:
         max_score = 0
 
         if y < l - 1:
-            score = self.score_outcome(self.id, grid[y+1][x], grid)
+            score = self.score_outcome(self.id, grid[y+1][x], grid) * self.enemy_on_tile (enemies, x,y+1)
             if score == max_score:
                 max_move+= [Move.UP]
             if score > max_score:
@@ -45,7 +48,7 @@ class Daniels_Bot:
                 max_score = score
                 
         if y > 0:
-            score = self.score_outcome(self.id, grid[y-1][x], grid)
+            score = self.score_outcome(self.id, grid[y-1][x], grid) * self.enemy_on_tile (enemies, x,y-1)
             if score == max_score:
                 max_move+= [Move.DOWN]
             if score > max_score:
@@ -53,7 +56,7 @@ class Daniels_Bot:
                 max_score = score
                 
         if x < l - 1:
-            score = self.score_outcome(self.id, grid[y][x+1], grid)
+            score = self.score_outcome(self.id, grid[y][x+1], grid) * self.enemy_on_tile (enemies, x+1,y)
             if score == max_score:
                 max_move+= [Move.RIGHT]
             if score > max_score:
@@ -61,46 +64,12 @@ class Daniels_Bot:
                 max_score = score
                  
         if x > 0:
-            score = self.score_outcome(self.id, grid[y][x-1], grid)
+            score = self.score_outcome(self.id, grid[y][x-1], grid) * self.enemy_on_tile (enemies, x-1,y+1)
             if score == max_score:
                 max_move+= [Move.LEFT]
             if score > max_score:
                 max_move = [Move.LEFT]
                 max_score = score       
 
-        # score = self.score_outcome(self.id, grid[y][x], grid)
-        # if score > max_score:
-            # max_move = Move.STAY
-            # max_score = score  
-
-        # Pick one of the possible moves randomly
+        # Pick one of the possible moves randomly to reduce getting stuck in a loop
         return max_move[random.randint(0, len(max_move) - 1)]
-
-#   def __init__(self):
-#       self.move_ydir = -1
-#       self.move_xdir = -1
-#
-#  def determine_next_move(self, grid, enemies, game_info):
-#      x = self.position[0]
-#      y = self.position[1]
-#      l = grid.shape[0] # always square
-#
-#      if y + self.move_ydir < l and y + self.move_ydir >= 0:
-#          if self.move_ydir > 0:
-#              return Move.UP
-#          else:
-#              return Move.DOWN
-#      else:
-#          self.move_ydir = self.move_ydir * -1
-#          if x + self.move_xdir < l and x + self.move_xdir >= 0:
-#              if self.move_xdir > 0:
-#                  return Move.RIGHT
-#              else:
-#                  return Move.LEFT
-#          else:
-#              self.move_xdir = self.move_xdir * -1
-#              if self.move_xdir > 0:
-#                  return Move.RIGHT
-#              else:
-#                  return Move.LEFT
-#          
